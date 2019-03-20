a2f4af47c92968966bf6f3e1ccdaa0bd6e5ff6c4
kevinkiklee <kevin.kik.lee@gmail.com>
2019-03-19 22:02:05 -0400

    Fail HTML implementation.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './app.css'
+import generateGrid from './generate-grid'
 
 class App extends Component {
+  generateGrid = () => {
+    const grid = generateGrid(300, ['100', '011', '010', '001'])
+    console.log({ grid });
+    return (
+      <tbody className='grid'>
+        {grid.map((row, i) =>
+          <tr className='row' key={i}>
+            {row.map((cell, j) =>
+              <td className={`cell ${cell ? 'cell--active' : 'cell--inactive'}`} key={j} />
+             )}
+          </tr>
+        )}
+      </tbody>
+    )
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='app'>
+        <div className='sidebar'>
+          <div className='sidebar__app-title'>
+            Elementary CA Visualizer
+          </div>
+          <div className='sidebar__app-description'>
+            Lorem ipsum blahblahbal plumbus
+          </div>
+        </div>
+        <table className='content'>
+          {this.generateGrid()}
+        </table>
       </div>
     );
   }
---
+++ src/generate-grid.js
@@ -0,0 +1,56 @@
+function* ecaGenerator(activePatterns) {
+  let state = [0, 1, 0]
+
+  while (true) {
+    yield state
+    state.unshift(0)
+    state.push(0)
+
+    const newState = []
+
+    for (let i = 0; i < state.length; i += 1) {
+      const pattern = `${state[i - 1] || 0}${state[i]}${state[i + 1] || 0}`
+      const newCell = activePatterns.includes(pattern) ? 1 : 0
+      newState.push(newCell)
+    }
+
+    state = newState
+  }
+}
+
+const generateState = (size, activePatterns) => {
+  const ecaGen = new ecaGenerator(activePatterns)
+
+  const state = []
+  for (let i = 0; i < size; i += 1) {
+    state.push(ecaGen.next().value)
+  }
+
+  return state
+
+  // const state = new Array(size)
+  // state.map(cell => ecaGen.next().value)
+}
+
+const padRow = (row, size) => {
+  while (row.length < size) {
+    row.push(0)
+    row.unshift(0)
+  }
+
+  if (size % 2 !== 0) {
+    row.pop()
+  }
+
+  return row
+}
+
+const generateGrid = (size, activePatterns) => {
+  const state = generateState(size, activePatterns)
+  const maxLength = state[state.length - 1].length
+  const grid = state.map(row => padRow(row, maxLength))
+
+  return grid
+}
+
+export default generateGrid
